8b0508c40f0a2b345364ba9632113ff12df05cf2
Aditya Bile <adityabile30602@gmail.com>
2024-03-12 09:19:09 +0530

    Create & Insert command in mysql

--- MERN/SQL/index.js
+++ MERN/SQL/index.js
@@ -10,7 +10,23 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query("SHOW TABLES", (err, result) => {
+let query1 = "SHOW TABLES";
+// connection.query(addUser, user1, (err, result) => {
+
+// Adding single user
+let addUser =
+  "INSERT INTO user (userID, username, email, password) VALUES (?,?,?,?)";
+let user1 = ["123", "user1", "user1@gmail.com", "user1"];
+// connection.query(addUser, user1, (err, result) => {
+
+// Inserting multiple records
+let addMultiUser =
+  "INSERT INTO user (userID, username, email, password)  VALUES ?";
+let multiUsers = [
+  ["456", "user2", "user2@gmail.com", "user2"],
+  ["789", "user3", "user3@yahoo.com", "user3"],
+];
+connection.query(addMultiUser, [multiUsers], (err, result) => {
   console.log(result);
   connection.end(); // Stop connection
 });
