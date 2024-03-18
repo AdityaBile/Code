f6ca69e63ecc4ab9b6b9ad624511534bcbf2b243
Aditya Bile <adityabile30602@gmail.com>
2024-03-18 22:36:44 +0530

    Callback in updater function

--- MERN/React/stateApp/src/Heart.jsx
+++ MERN/React/stateApp/src/Heart.jsx
@@ -10,18 +10,18 @@ const Heart = () => {
     setCount(count + 1);
   }
 
-  let setStyle = { color: "pink" };
+  let setStyle = { color: "red" };
 
   return (
     <>
-      <h2>Count : {count}</h2>
-      <h2 onClick={toggle} style={setStyle}>
+      <h1>Count : {count}</h1>
+      <h1 onClick={toggle} style={setStyle}>
         {like ? (
           <i className="fa-regular fa-heart"></i>
         ) : (
           <i className="fa-solid fa-heart"></i>
         )}
-      </h2>
+      </h1>
     </>
   );
 };
--- MERN/React/stateApp/src/UseState.jsx
+++ MERN/React/stateApp/src/UseState.jsx
@@ -1,5 +1,52 @@
 import { React, useState } from "react";
 
+// More about useState()
+// 2) Passing function to initialization
+
+function init() {
+  return Math.random();
+}
+
+const UseState = () => {
+  // Pass function as "init"-> (this will execute function once) not "init()" -> (this will execute multiple times)
+  const [count, setCount] = useState(init);
+  let incCount = () => {
+    setCount((count) => count + 1);
+  };
+
+  /*
+  // 1) callBack
+  const [count, setCount] = useState(0);
+  let incCount = () => {
+    
+    // setCount(count + 1);
+    // setCount(count + 1);
+    // setCount(count + 1);
+    
+    // Above will not increaement by 3 as useState depend on old value, here no value updation
+    // Use callback
+    // Now value will be updated by 2
+    setCount((currValue) => {
+      return currValue + 1;
+    });
+    setCount((currValue) => {
+      return currValue + 1;
+    });
+
+    
+  };
+  */
+  return (
+    <>
+      <h1>Count : {count}</h1>
+      <button onClick={incCount}>Increase Count</button>
+    </>
+  );
+};
+
+export default UseState;
+
+/* // UseState
 const UseState = () => {
   // count =>  state variable, setCount => function to update the state of count
   // useState(//initialValue);
@@ -18,6 +65,8 @@ const UseState = () => {
 };
 
 export default UseState;
+
+*/
 
 /*
 //  UseState()
