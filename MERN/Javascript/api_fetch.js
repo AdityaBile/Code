b4960fa4a500fab61fe1e0d3149f43b4ca068ced
Aditya Bile <adityabile30602@gmail.com>
2024-03-22 00:13:46 +0530

    update in api fetch - not imp

--- MERN/Javascript/api_fetch.js
+++ MERN/Javascript/api_fetch.js
@@ -1,33 +1,34 @@
-
-/*
-let url = `https://api.tvmaze.com/search/shows?q=all`;
+let url = `https://s3.amazonaws.com/roxiler.com/product_transaction.json`;
+let url1 = `https://api.tvmaze.com/search/shows?q=all`;
 
 fetch(url)
-.then((res) =>{
+  .then((res) => {
     console.log(res);
     return res.json();
-})
-.then((data) =>{
+  })
+  .then((data) => {
     console.log(data.score);
-})
-.catch((err)=>{
-    console.log("Error", err)});
-*/
+  })
+  .catch((err) => {
+    console.log("Error", err);
+  });
 
+/*
 // Async ------
 
-let url = `https://api.tvmaze.com/search/shows?q=all`;
+// let url = `https://api.tvmaze.com/search/shows?q=all`;
 
-async function getFact(){
-    try{
-        let res = await fetch(url);
-        let data = await res.json();
-        
-        console.log(data.show.id);
-    }
-    catch(e){
-        console.log("Erro4r",e);
-    }
+async function getFact() {
+  try {
+    let res = await fetch(url);
+    let data = await res.json();
+
+    // console.log(data.show.id);
+    console.log(data);
+  } catch (e) {
+    console.log("Erro4r", e);
+  }
 }
 
 getFact();
+*/
